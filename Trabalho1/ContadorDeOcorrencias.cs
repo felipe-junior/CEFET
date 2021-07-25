50fb959dc7e8711f65445eab0aad769a2810a96c
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-07-25 20:30:49 -0300

    Refatorando o código para usar a função ReadAllLines no lugar da ReadAllBytes

--- Trabalho1/ContadorDeOcorrencias.cs
+++ Trabalho1/ContadorDeOcorrencias.cs
@@ -7,76 +7,88 @@ namespace Arquivo
     class Program
     {
 
-        static public void UppercaseCounter(string text){
+        static public void UppercaseCounter(string[] text){
             Console.WriteLine("***** Numero de caracteres maiusculos*****");
             int counter = 0;
-            foreach (char character in text){
-                if(char.IsUpper(character)){
-                    counter++;
+            foreach (string line in text){
+                foreach(char character in line){
+                     if(char.IsUpper(character)){
+                        counter++;
+                    }
                 }
+                   
             }
             Console.Write(counter);
             Console.WriteLine();
         }
-        static public void LowercaseCounter(string text){
+        static public void LowercaseCounter(string[] text){
 
             Console.WriteLine("***** Numero de caracteres minusculos *****");
             int counter = 0;
-            foreach (char character in text){
-                if(char.IsLower(character)){
-                    counter++;
+            foreach (string line in text){
+                foreach(char character in line){
+                    if(char.IsLower(character)){
+                        counter++;
+                    }
                 }
+                
             }
             Console.Write(counter);
             Console.WriteLine();
         }
         
-        static public void DigitCounterInRange(string text, int begin, int end){
+        static public void DigitCounterInRange(string[] text, int begin, int end){
             Console.WriteLine("***** Numeros no range {0} e {1}*****", begin, end);
             int counter = 0;
-            foreach (char character in text){
-                if(char.IsDigit(character)){
-                    int digit = int.Parse(character.ToString());
-                    if(digit >= begin && digit <= end )
-                        counter++;
+            foreach (string line in text){
+                foreach(char character in line){
+                    if(char.IsDigit(character)){
+                        int digit = int.Parse(character.ToString());
+                        if(digit >= begin && digit <= end )
+                            counter++;
+                    }
                 }
             }
             Console.Write(counter);
             Console.WriteLine();
         }
-        static public void SpaceCounter(string text){
+        static public void SpaceCounter(string[] text){
 
             Console.WriteLine("***** Número de espaços *****");
+            
             int counter = 0;
-            foreach (char character in text){
-                if(char.IsWhiteSpace(character))
-                    counter++;
+            foreach (string line in text){
+                foreach(char character in line){
+                    if(char.IsWhiteSpace(character))
+                        counter++;
+                }
+                
             }
             Console.WriteLine(counter);
             Console.WriteLine();
         }
-        static public void BreakLineCounter(string text){
+        static public void BreakLineCounter(string[] text){
 
             Console.WriteLine("***** Numero de quebras de linhas *****");
-            int counter = 0;
-            foreach (char character in text){
-                if(character.CompareTo('\n') == 0)
-                    counter++;
-            }
-            Console.WriteLine(counter);
+            Console.WriteLine(text.Length); //O numero de linhas sempre coincide com o tamanho da matriz
             Console.WriteLine();
         }
         static void Main(string[] args)
         {
-            byte[] textByte =File.ReadAllBytes(args[0]); //Pega o numero de bytes do caminho q está no argumento
-            string text= Encoding.UTF8.GetString(textByte); // Converti para string porque estava dando problema com o Ç e outros q nao sao originarias da lingua inglesa
+
+            try{
+                string [] text = File.ReadAllLines(args[0], Encoding.UTF8);
+                UppercaseCounter(text);
+                LowercaseCounter(text);
+                DigitCounterInRange(text, 0, 9);
+                SpaceCounter(text);
+                BreakLineCounter(text);
             
-            UppercaseCounter(text);
-            LowercaseCounter(text);
-            DigitCounterInRange(text, 0, 9);
-            SpaceCounter(text);
-            BreakLineCounter(text);
-            //obs: O breakLine também eh contado como 2 espaços!
+            
+            } catch(IndexOutOfRangeException){
+                System.Console.WriteLine("Especifique o seu o caminho do seu arquivo no argumento na execução do codigo");
+            }
+
         }
     }
 }
