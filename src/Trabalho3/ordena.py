91d2584f31f70b5c3f4839c05fa4811b88bb5b3e
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-09-25 03:12:19 -0300

    Ordenamento de arquivo em partes menores e arquivo gerado dessas partes

---
+++ src/Trabalho3/ordena.py
@@ -0,0 +1,38 @@
+from io import SEEK_SET
+import struct
+import os
+
+caminho = "cep.dat"
+size_file = os.stat(caminho).st_size
+
+registroCEP = struct.Struct("72s72s72s72s2s8s2s")
+tamanhoDaLinha = registroCEP.size
+numeroDelinhas = size_file // registroCEP.size 
+numeroDivisoes = 8
+qtdLinhaPorDivisao = numeroDelinhas // numeroDivisoes 
+
+with open(caminho,"rb") as f: #lendo arquivo
+    for divisaoAtual in range(0, numeroDivisoes):
+        listaTemp= []
+        f.seek(divisaoAtual * qtdLinhaPorDivisao * tamanhoDaLinha, SEEK_SET) #faz o deslocamento do ponteiro do arquivo para a divisao pertinente
+        line = f.read(tamanhoDaLinha)
+        
+        i = 1
+        while(i <= qtdLinhaPorDivisao) and len(line)>0:
+            endereco = registroCEP.unpack(line) #Gera tuplas com base na struct definida
+            listaTemp.append(endereco) # adicicona o endereco a lista
+            line = f.read(tamanhoDaLinha)
+            i += 1
+        
+        listaTemp.sort(key=lambda e: e[5]) 
+        with open("cep_ordenado{}.dat".format(divisaoAtual),"wb") as file:
+            for endereco in listaTemp:
+                file.write(registroCEP.pack(*endereco))
+
+
+
+# lista.sort(key=lambda e: e[5])
+
+# with open("cep_ordenado.dat","wb") as f:
+#     for endereco in lista:
+#         f.write(registroCEP.pack(*endereco))
