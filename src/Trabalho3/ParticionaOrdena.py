eb91c6693cbc9e0b0121b9ad38fadba613b1e580
Felipe dos Santos <felipe-junior@users.noreply.github.com>
2021-09-25 19:37:49 -0300

    Particiona e ordena implementado e automatizado podendo escolher o numero de divisões

--- src/Trabalho3/ParticionaOrdena.py
+++ src/Trabalho3/ParticionaOrdena.py
@@ -8,14 +8,14 @@ size_file = os.stat(caminho).st_size
 registroCEP = struct.Struct("72s72s72s72s2s8s2s")
 tamanhoDaLinha = registroCEP.size
 numeroDelinhas = size_file // registroCEP.size 
-numeroDivisoes = 8
+numeroDivisoes = int(input("Numero de divisoes: "))
 qtdLinhaPorDivisao = numeroDelinhas // numeroDivisoes 
 
 with open(caminho,"rb") as f: #lendo arquivo
     for divisaoAtual in range(0, numeroDivisoes):
         listaTemp= []
         f.seek(divisaoAtual * qtdLinhaPorDivisao * tamanhoDaLinha, SEEK_SET) #faz o deslocamento do ponteiro do arquivo para a divisao pertinente
-        line = f.read(tamanhoDaLinha)
+        line = f.readline(tamanhoDaLinha)
         
         i = 1
         while(i <= qtdLinhaPorDivisao) and len(line)>0:
@@ -24,9 +24,9 @@ with open(caminho,"rb") as f: #lendo arquivo
             line = f.read(tamanhoDaLinha)
             i += 1
         
-        listaTemp.sort(key=lambda e: e[5]) 
+        listaTemp.sort(key=lambda e: e[5]) #ordena o arquivo gerado
         with open("cep_ordenado{}.dat".format(divisaoAtual),"wb") as file:
-            for endereco in listaTemp:
+            for endereco in listaTemp: 
                 file.write(registroCEP.pack(*endereco))
 
 
